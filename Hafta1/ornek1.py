21bb731d91387dfd8ba3cbcfedc4d91e5f4eb45f
Zafer <162111813+zafer-sn@users.noreply.github.com>
2025-07-19 22:07:45 +0300

    Create ornek1.py

---
+++ Hafta1/ornek1.py
@@ -0,0 +1,77 @@
+# Hafta-1 Örnekler
+"""
+Çok satırlı
+yorum satırı
+"""
+'''
+İkinci çok satırlı
+yorum satırı
+'''
+# built-in Python fonksiyonudur
+# numpy, pandas, torch
+# CTRL + Space
+# LLM -> Large Language Models
+print(3+5)
+print(3-2)
+print(3*2)
+print(3/2)
+print(4/2)
+print(3%2)
+print(3**3)
+# Variables - Değişkenler
+# Değer tipli değişken
+bilecikPlaka = 11
+bilecikPlaka = 34
+print(bilecikPlaka)
+
+izmirPlaka = 35
+adanaPlaka = 1
+# izmirPlaka = adanaPlaka # izmirPlaka = 1
+adanaPlaka = izmirPlaka # adanaPlaka = 35
+print(izmirPlaka)
+print(adanaPlaka)
+"""
+int -> integer -> tamsayı -> -4 milyar - +4milyar
+float -> floating point -> ondalıklı sayı
+bool -> boolean -> var-yok, 0-1, True-False
+string -> Metinsel ifadeleri tutmak için kullanılır
+"""
+yas = 10
+piSayisi = 3.14
+girisYapildimi = True
+isim = "Ahmet"
+print(type(yas))
+print(type(piSayisi))
+print(type(girisYapildimi))
+print(type(isim))
+print(f"Herkese merhaba, \nben {isim}") # \n -> newline -> yeni satır
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
